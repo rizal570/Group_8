088ffb9e44cabdec6d42b74574f34696e50ccf18
rizal570 <139453941+rizal570@users.noreply.github.com>
2024-07-09 22:36:19 +0700

    fix MAX30102_test.ino

--- Project_08/MAX30102_test/MAX30102_test.ino
+++ Project_08/MAX30102_test/MAX30102_test.ino
@@ -6,10 +6,10 @@
 const char* ssid = "POCO X";
 const char* password = "rizal111";
 const char* serverName = "http://192.168.69.205:5000/sensor/data";
-
 MAX30105 particleSensor;
 
-int counter;
+TaskHandle_t Task2;
+
 const byte RATE_SIZE = 4; //Increase this for more averaging. 4 is good.
 byte rates[RATE_SIZE]; //Array of heart rates
 byte rateSpot = 0;
@@ -63,10 +63,55 @@ void setup() {
 
   particleSensor.setPulseAmplitudeRed(0x0A); //Turn Red LED to low to indicate sensor is running
   particleSensor.setPulseAmplitudeGreen(0); //Turn off Green LED
+  //create a task that will be executed in the Task2code() function, with priority 1 and executed on core 0
+  xTaskCreatePinnedToCore(
+                    postData,   /* Task function. */
+                    "Task2",     /* name of task. */
+                    10000,       /* Stack size of task */
+                    NULL,        /* parameter of the task */
+                    1,           /* priority of the task */
+                    &Task2,      /* Task handle to keep track of created task */
+                    0);          /* pin task to core 1 */
+  delay(500);
 }
 
+void postData(void * pvParameters) {
+  Serial.print("Task2 running on core ");
+  Serial.println(xPortGetCoreID());
+
+  for(;;) {
+  delay(500);
+    
+    if (WiFi.status() == WL_CONNECTED) {
+    HTTPClient http;
+    http.begin(serverName);
+    http.addHeader("Content-Type", "application/x-www-form-urlencoded");
+
+    // Create form data
+    char httpRequestData[100];
+    int beatAvg_kirim = int(beatAvg);
+    int ESpO2_kirim = int(ESpO2);
+    sprintf(httpRequestData, "beatsPerMinute=%d&SpO2=%d", beatAvg_kirim, ESpO2_kirim);
+
+    int httpResponseCode = http.POST(httpRequestData);
+
+    if (httpResponseCode > 0) {
+      String response = http.getString();
+      Serial.println(httpResponseCode);
+      Serial.println(response);
+    } else {
+      Serial.print("Error on sending POST: ");
+      Serial.println(httpResponseCode);
+    }
+    http.end();
+  } else {
+    Serial.println("Error in WiFi connection");
+  }
+  }
+  
+
+}
 void loop() {
-  counter++;
   long irValue = particleSensor.getIR();    //Reading the IR value it will permit us to know if there's a finger on the sensor or not
   if (irValue > FINGER_ON ) {
      if (checkForBeat(irValue) == true) {
@@ -116,8 +161,6 @@ void loop() {
     
     if (beatAvg > 30)  Serial.println(",SPO2:" + String(ESpO2));
     else Serial.println(",SPO2:" + String(ESpO2));
-    postData(counter, counter+1);
-    delay(1000);
   }
   else {
     for (byte rx = 0 ; rx < RATE_SIZE ; rx++) rates[rx] = 0;
@@ -126,29 +169,4 @@ void loop() {
     SpO2 = 0; ESpO2 = 90.0;
    }
  
-}
-void postData(float beatPerMinute, double SPO2) {
-  if (WiFi.status() == WL_CONNECTED) {
-    HTTPClient http;
-    http.begin(serverName);
-    http.addHeader("Content-Type", "application/x-www-form-urlencoded");
-
-    // Create form data
-    char httpRequestData[100];
-    sprintf(httpRequestData, "beatsPerMinute=%f&SpO2=%f", beatsPerMinute, SpO2);
-
-    int httpResponseCode = http.POST(httpRequestData);
-
-    if (httpResponseCode > 0) {
-      String response = http.getString();
-      Serial.println(httpResponseCode);
-      Serial.println(response);
-    } else {
-      Serial.print("Error on sending POST: ");
-      Serial.println(httpResponseCode);
-    }
-    http.end();
-  } else {
-    Serial.println("Error in WiFi connection");
-  }
 }
